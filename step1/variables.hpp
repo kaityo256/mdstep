440fade2dcbe5f8475500ce2edc819a1c9c59e16
H. Watanabe <kaityo@users.sourceforge.jp>
2016-08-12 19:17:25 +0900

    adds energy.*

--- step1/variables.hpp
+++ step1/variables.hpp
@@ -4,16 +4,6 @@
 struct Atom {
   double qx, qy, qz;
   double px, py, pz;
-  Atom(void) {
-  }
-  Atom(double x, double y, double z) {
-    qx = x;
-    qy = y;
-    qz = z;
-    px = 0.0;
-    py = 0.0;
-    pz = 0.0;
-  }
 };
 //------------------------------------------------------------------------
 class Variables {
