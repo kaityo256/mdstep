7d88e47011d61ad366fd47c8d502874acda00730
H. Watanabe <kaityo@users.sourceforge.jp>
2016-08-13 22:38:49 +0900

    周期境界条件のinline化

--- step4/systemparam.hpp
+++ step4/systemparam.hpp
@@ -10,5 +10,13 @@ const double RC2 = 1.0 / CL2;
 const double RC6 = RC2 * RC2 * RC2;
 const double RC12 = RC6 * RC6;
 const double C0 = - 4.0 * (RC12 - RC6);
-void adjust_periodic(double &dx, double &dy, double &dz);
+inline void adjust_periodic(double &dx, double &dy, double &dz){
+  const double LH = L * 0.5;
+  if (dx < -LH)dx += L;
+  if (dx > LH) dx -= L;
+  if (dy < -LH)dy += L;
+  if (dy > LH) dy -= L;
+  if (dz < -LH)dz += L;
+  if (dz > LH) dz -= L;
+}
 //------------------------------------------------------------------------
